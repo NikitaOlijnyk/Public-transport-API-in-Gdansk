89b0d15cfbbc2f9c80dcc38b11750121b5813dba
Serhii <tustanovskiis@gmail.com>
2025-08-13 13:44:06 +0200

    fix: move date parser to the utils

---
+++ app/utils/parse_time.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+
+def parse_time_to_warsaw(timestr: str, tz):
+    if not timestr:
+        return None
+    try:
+        if timestr.endswith("Z"):
+            iso = timestr[:-1] + "+00:00"
+        else:
+            iso = timestr
+        dt = datetime.fromisoformat(iso)
+    except Exception:
+        return None
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    return dt.astimezone(tz)
